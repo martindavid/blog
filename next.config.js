7e189ebae15cf6137a4e509291b9af4fcc140518
Martin David Valentino <martin.valentino@cultureamp.com>
2024-07-28 14:15:02 +1000

    fix: update github action command

--- next.config.js
+++ next.config.js
@@ -89,7 +89,7 @@ module.exports = () => {
         },
       ]
     },
-    webpack: (config, options) => {
+    webpack: (config, _options) => {
       config.module.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
